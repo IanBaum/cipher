eb57fa867dc0b4c00c2aaabdc673eebf8d003f40
Ian Baum and Zach Hedgepeth <ian@>
2016-08-08 15:44:21 -0400

    add a function to reverse thestring

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,31 @@ var sentence = prompt("Enter a sentence!");
 console.log(sentence);
 
 var firstLast = function(sentence){
-return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length -1).toUpperCase();
+  return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length -1).toUpperCase();
 };
 
-alert(firstLast(sentence));
 
-var reverse = function(letters){
-return letters.charAt(1) + letters.charAt(0);
+var reverse = function(x){
+  return x.charAt(1) + x.charAt(0);
+};
+
+var letterReverse = function(sentence){
+  return sentence + reverse(firstLast(sentence));
+
+};
+
+var addMiddle = function(sentence){
+  return sentence.charAt(Math.floor(sentence.length/2)) + letterReverse(sentence);
+};
+
+console.log(addMiddle(sentence));
+
+var sentenceReverse = function(sentence){
+  var newsentence = "";
+  for(var i=sentence.length;i >= 0;i--){
+    newsentence += sentence.charAt(i);
+  }
+  return newsentence;
 }
+
+console.log(sentenceReverse(addMiddle(sentence)));
