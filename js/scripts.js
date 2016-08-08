4ffc379f296b94fa85df205bc711985a3236aa9c
Ian Baum and Zach Hedgepeth <ian@>
2016-08-08 14:23:59 -0400

    add a function to find the first and last letter and output them as capitals

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
 var sentence = prompt("Enter a sentence!");
 console.log(sentence);
+
+var firstLast = function(sentence){
+return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length -1).toUpperCase();
+};
+
+alert(firstLast(sentence));
